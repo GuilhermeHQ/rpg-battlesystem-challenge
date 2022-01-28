615acb7764e1b3bb05bea9bda9a0c0b6c6c43c15
guilherme_hq <quintana.guilherme@gmail.com>
2022-01-28 20:24:24 -0300

    enemy prefab setup

---
+++ Assets/Scripts/unit.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unit : MonoBehaviour
+{
+    public string unitName;
+    public string unitLevel;
+
+    public int damage;
+
+    public int maxHP;
+    public int currentHP;
+
+
+}
